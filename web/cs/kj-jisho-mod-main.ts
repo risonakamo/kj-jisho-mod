0cee95a247f42c41356de5f2808a131e70b2f2e7
risonakamo <hmykanae@gmail.com>
2026-03-21 17:13:09 -0400

    mods working

--- web/cs/kj-jisho-mod-main.ts
+++ web/cs/kj-jisho-mod-main.ts
@@ -1,6 +1,12 @@
+import {searchPageHide, wordPageLinkShortcuts} from "@/lib/jisho";
+
 function main():void
 {
-    console.log("hello");
+    console.log("applying search page mods");
+    searchPageHide();
+
+    console.log("applying word page mods");
+    wordPageLinkShortcuts();
 }
 
 main();
---
+++ web/lib/jisho.ts
@@ -0,0 +1,52 @@
+// funcs for modding jisho site
+
+/** insert style block to hide items on search page
+ *  for use on jisho search pages https://jisho.org/search/*
+ */
+export function searchPageHide():void
+{
+    const style:HTMLStyleElement=document.createElement("style");
+
+    style.textContent=`
+        .concept_light-representation .furigana {
+            opacity: 0;
+            transition: opacity 0.2s ease;
+        }
+
+        .concept_light-representation:hover .furigana {
+            opacity: 1;
+        }
+
+        .concept_light-meanings {
+            opacity: 0;
+        }
+
+        .concept_light-meanings:hover {
+            opacity: 1;
+        }
+    `;
+
+    document.head.appendChild(style);
+}
+
+/** insert quick links for searching for common words of kanjis on search page */
+export function wordPageLinkShortcuts():void
+{
+    const kanjiBoxes:NodeListOf<Element>=document.querySelectorAll(".entry");
+
+    kanjiBoxes.forEach((kanjiBox:Element)=>{
+        const kanji:string|undefined=kanjiBox.querySelector(".literal")?.textContent;
+
+        if (!kanji)
+        {
+            console.log("kanji text extraction problem");
+            return;
+        }
+
+        kanjiBox.insertAdjacentHTML("beforeend",`
+            <a href="https://jisho.org/search/*${kanji}*" class="light-details_link">
+                Word Search
+            </a>
+        `);
+    });
+}
